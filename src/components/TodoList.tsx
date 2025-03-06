6f2483ed407bfb138a25f3cdaddf79928891a574
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-03-06 23:20:23 +0100

    adjust css list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,24 +2,24 @@ function TodoList({ tasks, deleteTask, completeTask }) {
   console.log('yoo:', tasks);
 
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center   '>
       {/* {Render task NOT done:} */}
       {tasks
         .filter((task) => !task.done)
         .map((task, index) => (
           <div
             key={task.id}
-            className='flex flex-col gap-4 justify- items-center p-4 '
+            className='flex flex-col gap-4 justify-center items-center p-4 '
           >
-            <div className='flex justify-center items-center gap-4 '>
+            <div className='flex justify-center items-center gap-4  '>
               <input
                 onChange={() => completeTask(task.id)}
                 checked={task.done}
-                className='w-6 h-6 rounded-full appearance-none border-2 border-gray-400 checked:bg-secondary checked:border-gray-400 focus:outline-none cursor-pointer transition duration-200'
+                className='w-6 h-6  rounded-full appearance-none border-2 border-gray-400 checked:bg-secondary checked:border-gray-400 focus:outline-none cursor-pointer transition duration-200'
                 type='checkbox'
               />
 
-              <h2 className='text-xl '>{task.todo}</h2>
+              <h2 className='text-xl break-all w-full'>{task.todo}</h2>
             </div>
             <div className='flex justify-center items-center gap-4'>
               <p className='text-gray-500'>{task.date}</p>
@@ -32,8 +32,9 @@ function TodoList({ tasks, deleteTask, completeTask }) {
             </div>
           </div>
         ))}
+
       {/* {Render task done:} */}
-      <hr className='m-auto w-3xl my-8 border-gray-300' />
+      <hr className='w-full max-w-3xl m-auto my-8 border-gray-300' />
       <p className='flex justify-center opacity-50 mt-4'>
         {tasks.filter((task) => task.done).length > 0 ? (
           <>Completed: {tasks.filter((task) => task.done).length}</>
@@ -55,10 +56,7 @@ function TodoList({ tasks, deleteTask, completeTask }) {
                 type='checkbox'
               />
 
-              <h2
-                className='text-xl line-through text-gray-500
-'
-              >
+              <h2 className='text-xl line-through text-gray-500 break-all w-full'>
                 {task.todo}
               </h2>
             </div>
